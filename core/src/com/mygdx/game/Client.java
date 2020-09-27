32adf3f743195ae1c7d937fe4f3537af5b03e5c7
Mihai Hudisteanu <hudisteanum@yahoo.com>
2020-09-27 13:07:46 +0300

    update

---
+++ core/src/com/mygdx/game/Client.java
@@ -0,0 +1,338 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.mygdx.game.Screens.CreateRoom;
+import com.mygdx.game.Screens.Credentials;
+import com.mygdx.game.Screens.GameScreen;
+import com.mygdx.game.Screens.JoinRoom;
+import com.mygdx.game.Screens.Lobby;
+import com.mygdx.game.Screens.MainMenu;
+import com.mygdx.game.networking.dto.NetworkDTO;
+import com.mygdx.game.networking.networkController.NetworkController;
+import com.mygdx.game.networking.networkService.NetworkService;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Client extends Game implements NetworkController {
+
+	//todo sa nu lesi asta aici
+	public String nickName = "Hudy";
+
+	public float screenWidth;
+	public float screenHeight;
+
+	public ScreenController screenController;
+
+	public NetworkService networkService;
+
+	MainMenu mainMenuScreen;
+	Credentials credentialsScreen;
+	JoinRoom joinRoomScreen;
+	CreateRoom createRoomScreen;
+	Lobby lobbyScreen;
+	GameScreen gameScreen;
+
+
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		System.out.println("Disposed here!");
+	}
+
+	@Override
+	public void create() {
+		// System.out.println("Version: " + version);
+
+		initializeNetworkService(Constants.serverHTTP);
+		screenController = new ScreenController(this);
+
+
+		mainMenuScreen =  new MainMenu(this);
+		credentialsScreen =  new Credentials(this);
+		joinRoomScreen =  new JoinRoom(this);
+		createRoomScreen = new CreateRoom(this);
+		lobbyScreen = new Lobby(this);
+		gameScreen =  new GameScreen(this);
+
+
+		setScreen(gameScreen);
+
+		try {
+			connect();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void setSCreen(Screen screen){
+		setScreen(screen);
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+
+	public float getScreenWidth() {
+		return screenWidth;
+	}
+
+	public void setScreenWidth(float screenWidth) {
+		this.screenWidth = screenWidth;
+	}
+
+	public float getScreenHeight() {
+		return screenHeight;
+	}
+
+	public void setScreenHeight(float screenHeight) {
+		this.screenHeight = screenHeight;
+	}
+
+	/**
+	 * Get a copy of the instance of Network interface
+	 * ex :
+	 * {
+	 * NetworkService service = NetworkService.getInstance();
+	 * service.initService(http,this)
+	 * this.networkService = service;
+	 * }
+	 *
+	 * @param http : the address of the server (host + port)
+	 * @return -;
+	 */
+	@Override
+	public void initializeNetworkService(String http) {
+		NetworkService service = NetworkService.getInstance();
+	 	service.initService(http,this);
+		this.networkService = service;
+	}
+
+	@Override
+	public void connect() throws URISyntaxException {
+		this.networkService.login();
+	}
+
+	/**
+	 * This function gets call when the connection status has changed
+	 * ex: when you try to connect to server, when to connection is establish... the parameter isConnected will be true;
+	 *
+	 * @param isConnected
+	 */
+	@Override
+	public void setConnectedStatus(boolean isConnected) {
+	}
+
+	/**
+	 * This function gets called when the networkingService has received token
+	 *
+	 * @param token
+	 */
+	@Override
+	public void setToken(NetworkDTO.Token token) {
+
+	}
+
+	/**
+	 * This function get's called when the player made the ROOM_REQUEST. It moves the screen to ROOM_SCREEN
+	 */
+	@Override
+	public void goToRoomsScreen() {
+
+	}
+
+	/**
+	 * This function get's called when the client received the response from ROOM_REQUEST. Look to put all the rooms in the ROOM_SCREEN.
+	 *
+	 * @param roomsResponse
+	 */
+	@Override
+	public void updateRooms(NetworkDTO.RoomsResponse roomsResponse) {
+
+	}
+
+	/**
+	 * This functions gets called every time client receives lobbyData
+	 *
+	 * @param lobby
+	 * @param isOwner -> true === u are owner, false otherwise;
+	 */
+	@Override
+	public void setLobbyData(NetworkDTO.Lobby lobby, boolean isOwner) {
+
+	}
+
+	/**
+	 * This function gets called when you need to change the screen to LOBBY
+	 */
+	@Override
+	public void goToLobbyScreen() {
+
+	}
+
+	/**
+	 * This function gets called when you need to change the screen to MAIN_MENU
+	 */
+	@Override
+	public void goToMainMenu() {
+
+	}
+
+	/**
+	 * This function gets called when you need to change the screen to GAME
+	 */
+	@Override
+	public void goToGame() {
+
+	}
+
+	/**
+	 * This functions gets called when you received the players for a game for the first time
+	 *
+	 * @param players
+	 */
+	@Override
+	public void updatePlayerList(List<NetworkDTO.Player> players) {
+
+	}
+
+	/**
+	 * This function gets called when you received the cards
+	 *
+	 * @param cards
+	 */
+	@Override
+	public void updateCards(NetworkDTO.Cards cards) {
+
+	}
+
+	/**
+	 * This function gets called when it's your turn to make the bid
+	 * It's up to you to ensure that the bid which will be made is valid!!
+	 *
+	 * @param bid
+	 */
+	@Override
+	public void showHudForBids(NetworkDTO.Bids.Bid bid) {
+
+	}
+
+	/**
+	 * This function gets called when it's not your turn to make bid;
+	 */
+	@Override
+	public void hideBidHUD() {
+
+	}
+
+	/**
+	 * This function gets called when it's your turn to place a card
+	 * It's up to you to ensure that the card which will be placed is valid;
+	 *
+	 * @param ps
+	 */
+	@Override
+	public void showHudForCards(NetworkDTO.Table.PlayerStatus ps) {
+
+	}
+
+	/**
+	 * This function gets called when it's not your turn to place a card
+	 */
+	@Override
+	public void hideCardHud() {
+
+	}
+
+	/**
+	 * This function gets called when you received the bids Data
+	 *
+	 * @param bids
+	 */
+	@Override
+	public void updateBids(NetworkDTO.Bids bids) {
+
+	}
+
+	/**
+	 * This function gets called when the client received Table Data
+	 *
+	 * @param table
+	 */
+	@Override
+	public void updateTable(NetworkDTO.Table table) {
+
+	}
+
+	/**
+	 * Call this function when you picked a card and want to send it to server
+	 * !! Make sure the card is valid before calling this function!!
+	 *
+	 * @param card
+	 */
+	@Override
+	public void sendCard(String card) {
+
+	}
+
+	/**
+	 * Call this function when you choosed the bid and want to send it to server
+	 * !!Make sure the bid is valid before calling this function!!
+	 *
+	 * @param bid
+	 */
+	@Override
+	public void sendBid(int bid) {
+
+	}
+
+	/**
+	 * Call this function when you want to join a game from the room_list
+	 *
+	 * @param roomID
+	 * @param nickname
+	 */
+	@Override
+	public void joinGame(String roomID, String nickname) {
+
+	}
+
+	/**
+	 * Call this function when you want to start the game in which you are owner;
+	 */
+	@Override
+	public void startGame() {
+
+	}
+
+	/**
+	 * Call this function when you want to leave the room
+	 */
+	@Override
+	public void leaveRoom() {
+
+	}
+
+	/**
+	 * call this function when you want to get all the rooms
+	 */
+	@Override
+	public void getRooms() {
+
+	}
+
+	/**
+	 * Call this function when you want to disconnect to the server
+	 * EX: When the player press the exit button or shut down the main procces.
+	 */
+	@Override
+	public void disconnect() {
+
+	}
+}
+
---
+++ core/src/com/mygdx/game/Screens/Lobby.java
@@ -0,0 +1,164 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Client;
+import com.mygdx.game.Constants;
+import com.mygdx.game.ScreenController;
+import com.mygdx.game.generics.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lobby implements Screen {
+    Stage stage;
+    Skin skin;
+    Viewport viewport;
+    float width;
+    float height;
+
+    Client mainController;
+    ScreenController screenController;
+
+    //Buttons
+
+
+
+    public List<Player> players = new ArrayList<>();
+    public Table table = new Table();
+
+    public Lobby(Client mainController){
+        viewport = new ScreenViewport();
+        stage = new Stage(viewport);
+        this.mainController = mainController;
+        screenController =  mainController.screenController;
+        System.out.println( " Created Lobby Screeem ");
+    }
+
+
+    @Override
+    public void show() {
+        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight()));
+        skin = new Skin(Gdx.files.internal(Constants.skinJsonString));
+        Gdx.input.setInputProcessor(stage);
+
+        table.debug();
+
+        table.center();
+
+        table.defaults().expandX().fill().space(5f);
+
+        Player pl1 = new Player("hudy",2);
+        Player pl2 = new Player("hudy1",2);
+        Player pl3 = new Player("hudy2",2);
+        Player pl4 = new Player("hudy3",2);
+        Player pl5 = new Player("hudy4",2);
+
+        players.add(pl1);
+        players.add(pl2);
+        players.add(pl3);
+        players.add(pl4);
+        players.add(pl5);
+
+        refreshTable(table,players);
+
+        ScrollPane scrollPane = new ScrollPane(table,skin);
+        scrollPane.setWidth(Gdx.graphics.getWidth()-200);
+        scrollPane.setHeight(Gdx.graphics.getHeight()-100);
+        scrollPane.setPosition(50,50);
+        scrollPane.debug();
+
+        TextButton startBtn = new TextButton("Start",skin);
+        startBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println( "Start Button Pressed");
+                screenController.goToGameScreen();
+            }
+        });
+        startBtn.setHeight(30);
+        startBtn.setWidth(100);
+        startBtn.setPosition(Gdx.graphics.getWidth() - startBtn.getWidth() - 20,20);
+
+        TextButton backBtn = new TextButton("Back",skin);
+        backBtn.setPosition(15,15);
+        backBtn.setHeight(30);
+        backBtn.setWidth(100);
+
+        backBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                screenController.goToMainMenu();
+            }
+        });
+         //stage.addActor(table);
+        stage.addActor(backBtn);
+        stage.addActor(scrollPane);
+        stage.addActor(startBtn);
+
+    }
+
+    public void refreshTable(Table table, List<Player> players){
+        table.clear();
+        table.defaults().width(110);
+
+        for (Player pl:players) {
+            table.row().setActorHeight(20);
+            Label label = new Label(pl.getNickName() + " ",skin);
+            label.setAlignment(Align.center);
+            table.add(label);
+            table.row();
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.8f,0.8f, 0.8f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+
+        refreshTable(table,players);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
