dfbd84dbff7d21aac052ee7fc51678e0054b0c40
Odrin Oprisanu <odrinoprisanu@gmail.com>
2020-10-04 14:59:39 +0300

    reformat Cards

---
+++ core/src/com/mygdx/game/testing/GameScreenTest.java
@@ -0,0 +1,105 @@
+package com.mygdx.game.testing;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.game.businessLayer.others.Constants;
+import com.mygdx.game.dataLayer.generics.Card;
+import com.mygdx.game.dataLayer.repositories.CardsTextureRepository;
+
+public class GameScreenTest implements Screen {
+
+    Stage stage;
+    Skin skin;
+    CardsTextureRepository cardsTextureRepository;
+
+
+    public GameScreenTest(CardsTextureRepository cardsTextureRepository) {
+        this.cardsTextureRepository = cardsTextureRepository;
+    }
+
+    @Override
+    public void show() {
+        skin = new Skin(Gdx.files.internal(Constants.skinJsonString));
+        stage = new Stage();
+
+
+
+
+
+        CardTest1 c = new CardTest1(cardsTextureRepository.getCardTexture("back"),cardsTextureRepository.getCardTexture("h-14"));
+        c.addListener(new ClickListener());
+
+        stage.addActor(c);
+
+
+        TextButton flipButton = new TextButton("Flip",skin);
+
+
+        flipButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+                for (Actor a: stage.getActors()){
+                    if(a instanceof CardTest1){
+                        CardTest1 localCard = (CardTest1) a;
+                        localCard.setFlipped(!localCard.isFlipped());
+                    }
+                }
+            }
+        });
+
+
+        stage.addActor(flipButton);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.8f,0.8f, 0.8f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+
+    @Override
+    public void pause() {
+
+    }
+
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+    }
+}
